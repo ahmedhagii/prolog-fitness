dd5a2c252313fc2d9ef8e6d571d9daabbf31214d
Ahmed Akram <a.akram93@gmail.com>
2016-01-13 06:35:32 +0300

    fixing some bugs regarding requests and putting constraints on info fields

--- nodejs-frontend/app/app.js
+++ nodejs-frontend/app/app.js
@@ -13,13 +13,23 @@ angular.module('myApp', [
 
 .controller('RootCtrl', ['$location', '$scope', '$http', 'usSpinnerService', function($location, $scope, $http, usSpinnerService) {
 
-	var activit_level;
-	var purpose;
+	var activit_level=null;
+	var purpose=null;
 
 	// $scope.meals = [{number:1, name:'mealone'}, {number:2, items:[{name:'chicken', number:50}, {name:'beef', number:50}]},{number:3, items:[{name:'beef'}]}]
-	var res = []
-
+	var res = [];
+	$scope.mealsNumber=null;
 	$scope.beastify = function beastify() {
+
+		if($scope.mealsNumber == null || $scope.mealsNumber == "" || $scope.weight==null || $scope.weight == "" || $scope.fatPerc == null || $scope.fatPerc == ""
+			|| purpose == null || activit_level == null || !($("input:checkbox[id='Bulking']")[0].checked || $("input:checkbox[id='Losing']")[0].checked)) {
+			console.log($scope.mealsNumber);
+			console.log($scope.weight);
+			console.log($scope.fatPerc);
+			console.log(activit_level);
+			alert("all fields must be filled");
+			return;
+		}
 
 		usSpinnerService.spin('spinner-1');
 		var activit_num;
@@ -44,6 +54,7 @@ angular.module('myApp', [
 
 		callMe(5, data);
 		var allData = []
+		var failedAttempts=0;
 		function callMe(Days, data) {
 			if(Days == 0) {
 	        	usSpinnerService.stop('spinner-1');
@@ -57,11 +68,16 @@ angular.module('myApp', [
 	            data: data,
 	        }).then(function (response) {
 	        	console.log(response.data);
-
 	        	if(response.data.indexOf("Error") > -1 || response.data.indexOf("Time limit") > -1) {
-	        		callMe(Days, data);
+		        	failedAttempts += 1;
+		        	if(failedAttempts > 3) {
+		        		callMe(Days-1, data);
+		        	}else {
+		        		callMe(Days, data);
+		        	}
 	        		// alert(response.data);
 	        	}else {
+	        		failedAttempts = 0;
 	        		allData.push(response.data);
 	        		callMe(Days-1, data);
 	        	}
