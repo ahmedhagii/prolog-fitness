6af771f4dd0b716af1769c168e3e06b72406231a
Ahmed Akram <akram@Ahmeds-MacBook-Pro.local>
2017-03-09 09:23:29 +0200

    fixing some nutrition info

--- prolog-backend/database.pl
+++ prolog-backend/database.pl
@@ -3,8 +3,8 @@ food(breakfast, 2, ('Swiss cheese', 'swiss_cheese', 27000, 5000, 28000 ,380000,2
 food(breakfast, 2, ('Cheddar Cheese', 'cheddar_cheese', 25000, 1300, 33000 ,402000,28,100)).
 food(breakfast, 2, ('Gouda Cheese', 'gouda_cheese', 25000, 2200, 27000 ,356000,28,100)).
 food(breakfast, 3, ('Multi Grain Bread', 'grain_bread', 13000, 43000, 4200 ,265000,24,96)).
-food(breakfast, 4, ('Whole Egg (Uncooked)', 'egg', 13000, 700, 10000 ,143000,1,3)).
-food(breakfast, 4, ('Egg Whi tes (Uncooked)', 'egg', 11000, 700, 200 ,52000,1,5)).
+food(breakfast, 4, ('Whole Egg', 'egg', 13000, 700, 10000 ,143000,1,3)).
+food(breakfast, 4, ('Egg Whites', 'egg', 11000, 700, 200 ,52000,1,5)).
 food(breakfast, 5, ('Skimmed Milk', 'milk',3400, 5000, 100 ,34000,100,500)).
 food(breakfast, 5, ('Milk full fat', 'milk', 3300, 4600, 3700 ,64000,100,500)).
 food(breakfast, 6, ('Whey Protein', 'whey', 80000, 10000, 3300 ,400000,30,60)).
@@ -13,31 +13,38 @@ food(breakfast-lunch, 1, ('Banana ', 'banana', 1100, 23000, 300 ,89000,100,300))
 food(breakfast-lunch, 2, ('Pear', 'pear', 400, 15000, 100 ,57000,150,300)).
 food(breakfast-lunch, 3,('Peanut butter', 'peanut', 25000, 20000, 50000 ,588000,16,48)).
 food(breakfast-lunch, 4, ('Multi Grain Bread', 'grain_bread', 13000, 43000, 4200 ,265000,24,96)).
+food(breakfast-lunch, 5, ('Honey', 'honey', 300, 82000, 0 ,304000,5,25)).
 
-food(lunch, 1, ('Fish', 'fish', 19000, 0, 6000 ,134000,100,600)).
-food(lunch, 1, ('Shrimp (Uncooked)', 'shrimp', 20000, 0, 500 ,85000,100,600)).
-food(lunch, 1, ('Lean Beef (Uncooked)', 'beef', 26000, 0, 15000, 250000,100,600)).
-food(lunch, 1, ('Salmon (Uncooked)', 'salmon', 20000, 0, 13000 ,208000,100,600)).
-food(lunch, 1, ('Chicken Breast (Uncooked)', 'chicken_breast', 31000, 0, 3600, 165000,100,600)).
-food(lunch, 2, ('White Rice (Uncoooked)', 'white_rice', 7000, 82000, 600 ,370000,70,300)).
-food(lunch, 2, ('Pasta (Uncooked)', 'pasta', 13000, 75000, 1500 ,371000,70,300)).
-food(lunch, 3, ('Potato (Uncooked)', 'potato', 2000, 17000, 100 ,77000,170,400)).
-food(lunch, 4,('Spinach', 'spinach', 2900, 3600, 400 ,23000,50,250)).
+food(lunch, 1, ('Fish', 'fish', 23000, 0, 1600, 11000, 100,600)).
+food(lunch, 1, ('Shrimp', 'shrimp', 24000, 200, 300, 99000, 100,600)).
+food(lunch, 1, ('Lean Beef', 'beef', 26000, 0, 14000, 240000, 100,600)).
+food(lunch, 1, ('Salmon', 'salmon', 22000, 0, 12000, 206000, 100,600)).
+food(lunch, 1, ('Chicken Breast', 'chicken_breast', 31000, 0, 3600, 165000,100,600)).
+food(lunch, 2, ('Pasta', 'pasta', 5000, 25000, 1100 , 131000,70,300)).
+food(lunch, 2, ('White Rice', 'white_rice', 2400, 30000, 200 ,130000,70,300)).
+food(lunch, 3, ('Potato', 'potato', 2000, 17000, 100 ,77000,170,400)).
+food(lunch, 4, ('Spinach', 'spinach', 2900, 3600, 400 ,23000,50,250)).
 food(lunch, 5, ('Olive Oil', 'olive_oil', 0, 0, 100000 ,884000,4,20)).
 food(lunch, 6, ('Tomatos ', 'tomatos', 900, 3900, 200 ,18000,50,250)).
 
-food(lunch-dinner, 1,('Peanut butter', 'peanut', 25000, 20000, 50000 ,588000,16,48)).
+food(lunch-dinner, 1, ('Peanut butter', 'peanut', 25000, 20000, 50000 ,588000,16,48)).
 food(lunch-dinner, 2, ('Banana ', 'banana', 1100, 23000, 300 ,89000,100,300)).
 food(lunch-dinner, 3, ('Pear', 'pear', 400, 15000, 100 ,57000,150,300)).
 food(lunch-dinner, 4, ('Multi Grain Bread', 'grain_bread', 13000, 43000, 4200 ,265000,24,96)).
+food(lunch-dinner, 5, ('Honey', 'honey', 300, 82000, 0 ,304000,5,25)).
+food(lunch-dinner, 6, ('Whey Protein', 'whey', 80000, 10000, 3300 ,400000,30,60)).
 
 food(dinner, 1, ('Tuna (Brine)',  'tuna', 26000, 0, 1000 ,116000,50,200)).
-food(dinner, 2,  ('Broccoli', 'broccoli', 2800, 7000, 400 ,34000,50,250)).
+food(dinner, 2, ('Broccoli', 'broccoli', 2800, 7000, 400 ,34000,50,250)).
 food(dinner, 3, ('Carrots', 'carrots', 900, 10000, 200 ,41000,50,250)).
 food(dinner, 4, ('Olive Oil', 'olive_oil', 0, 0, 100000 ,884000,4,20)).
 food(dinner, 4, ('Tomatos ', 'tomatos', 900, 3900, 200 ,18000,50,250)).
-food(dinner, 5,  ('Honey', 'honey', 300, 82000, 0 ,304000,5,50)).
-food(dinner, 6,  ('Multi Grain Bread', 'grain_bread', 13000, 43000, 4200 ,265000,24,96)).
-food(dinner, 7,  ('Cheddar Cheese', 'cheddar_cheese', 25000, 1300, 33000 ,402000,28,150)).
-food(dinner, 7,  ('Gouda Cheese', 'gouda_cheese', 25000, 2200, 27000 ,356000,28,150)).
-food(dinner, 7,  ('Swiss cheese', 'swiss_cheese', 27000, 5000, 28000 ,380000,28,150)).
+food(dinner, 5, ('Honey', 'honey', 300, 82000, 0 ,304000,5,50)).
+food(dinner, 6, ('Multi Grain Bread', 'grain_bread', 13000, 43000, 4200 ,265000,24,96)).
+food(dinner, 7, ('Cheddar Cheese', 'cheddar_cheese', 25000, 1300, 33000 ,402000,28,150)).
+food(dinner, 7, ('Gouda Cheese', 'gouda_cheese', 25000, 2200, 27000 ,356000,28,150)).
+food(dinner, 7, ('Swiss cheese', 'swiss_cheese', 27000, 5000, 28000 ,380000,28,150)).
+food(dinner, 8, ('Whole Egg', 'egg', 13000, 700, 10000 ,143000,1,3)).
+food(dinner, 8, ('Egg Whites', 'egg', 11000, 700, 200 ,52000,1,5)).
+food(dinner, 9, ('Skimmed Milk', 'milk',3400, 5000, 100 ,34000,100,500)).
+food(dinner, 9, ('Milk full fat', 'milk', 3300, 4600, 3700 ,64000,100,500)).
